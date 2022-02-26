8a6f1e9bf51f74ebd5326860f2ffcc78602d5591
lifan <lf19950801@gmail.com>
2022-02-26 09:17:55 +0000

    提交注释

--- sanic/test1.py
+++ sanic/test1.py
@@ -17,4 +17,6 @@ async def main_page(request):
     return json({'id': '001', 'name': [[1, 2],
                                        3, 4]})
 
-app.run(host='127.0.0.1', port='8000', debug=True)
+# 0.0.0.0 本机127.0.0.1和外网ip访问都可以
+# 127.0.0.1 只有本机127.0.0.1才能访问
+app.run(host='0.0.0.0', port='8000', debug=True)
