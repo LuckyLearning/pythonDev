2c802dcb71d521a357a536c2c84b360aa60d70af
lifan <798449106@qq.com>
2022-03-05 18:17:00 +0800

    rasa初始化

--- requests/test.py
+++ requests/test.py
@@ -1,7 +1,7 @@
 import requests
 
 if __name__ == '__main__':
-    query = input("输入一个你选择的明显：")
+    query = input("输入一个你选择的明星：")
     url = f'https://www.sogou.com/web?query={query}'
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Edg/98.0.1108.56"
